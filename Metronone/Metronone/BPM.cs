78bda1dbcf8eaeb5ade9da8c0a829340c3ae7f0f
xTooth <jamsen.kim@gmail.com>
2017-11-21 21:53:52 +0200

    Welp.
    Work in progress

---
+++ Metronone/Metronone/BPM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Metronone
+{
+    class BPM
+    {
+        private static bool running = false;
+        private static bool thisIsWronk = true;
+        private static Thread elrunnerino = new Thread(() => yes());
+        private static int bpm = 100;
+        public BPM()
+        {
+            elrunnerino.Start();
+            
+        }
+        public void StartM()
+        {
+            running = true;
+            
+        }
+        public void StopM()
+        {
+            running = false;
+            
+        }
+        public void SetBpm(int prkl)
+            {
+            bpm = prkl;
+            }
+
+        public void ThisHurtsMyInsides()
+        {
+        thisIsWronk = false;
+        }
+
+        public static void yes()
+        {
+            while (thisIsWronk)
+            {
+
+                while (running)
+                {
+                    Console.Beep(4000, 100);
+                    Thread.Sleep(60000 / bpm);
+                }
+            }
+        }
+
+    }
+}
---
+++ Metronone/Metronone/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Metronone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BPM timer = new BPM();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Text.Text.Equals("Start"))
+            {
+                Text.Text = "Stop";
+                timer.StartM();
+
+            }
+            else
+            {
+                Text.Text = "Start";
+                timer.StopM();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            timer.StopM();  //spaghettisolutions...
+            timer.ThisHurtsMyInsides();  // I hate this. Solid HotFix.
+            this.Close();
+        }
+
+        private void Background_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void BotTime_DropDownClosed(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
